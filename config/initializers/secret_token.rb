369eef01e73931fa2ca4db28d6bf478add1386ed
varun-singh9786 <varun.singh9786@gmail.com>
2013-12-17 22:50:33 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fed::Application.config.secret_key_base = '9e9c3df3f623df029570d0df2d4526596d2e235c18ee9df65f2fa14d47b5c938248ec4e40294963da22c35034902d5f972b7c5098b0a57f7983b688130d14ab0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fed::Application.config.session_store :cookie_store, key: '_fed_session'
