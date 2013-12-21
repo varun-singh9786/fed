761f95fdc5ca5b2d0806b0bd0480dae98cee2f28
varun-singh9786 <varun.singh9786@gmail.com>
2013-12-21 20:13:34 +0530

    fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Fed::Application.routes.draw do
   resources :users do
     resources :event_entries
-    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
