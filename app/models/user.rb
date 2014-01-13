0a4ca76705d6f7c3c8721b53b3c742db047baab7
U-HOME-7D7AC31BBA\Home <Home@home-7d7ac31bba.(none)>
2014-01-13 23:22:00 +0530

    - implementing token based auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ActiveRecord::Base
   has_many :food_entries, dependent: :destroy
   validates :name, presence: true, length: {minimum: 3}
   validates :email, presence: true, length: {minimum: 3}, uniqueness: {case_sensitive: false}
+  before_create :create_remember_token
+
+
+
+  def User.new_random_token
+  	SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token) 
+  	Digest::SHA1.hexdigest(token.to_s)
+  end
+
+private
+  def create_remember_token
+  	self.remember_token = User.encrypt(User.new_random_token)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131226100025) do
+ActiveRecord::Schema.define(version: 20140113172406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20131226100025) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token"
   end
 
 end
